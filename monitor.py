eafd9dc7db02aec707a77891c7a0757fbf5da938
Konstantin Sering <konstantin.sering@gmail.com>
2012-07-11 19:10:01 +0200

    added example sections to docstrings with small example code in every module, tried to remove all trailing whitespaces

--- monitor.py
+++ monitor.py
@@ -12,7 +12,7 @@
 # output: --
 #
 # created 2012-05-29 KS
-# last mod 2012-06-11 09:58 KS
+# last mod 2012-07-11 18:52 KS
 
 """
 This module provides the class Monitor, which capsulates the presentation
@@ -25,7 +25,7 @@ color and collect key presses.
 
 from psychopy import visual, event, core
 
-class E(object):
+class Event(object):
     """
     very small class to store a key stroke
 
@@ -44,15 +44,20 @@ class Monitor(object):
     Monitor provides a convenient interface to present a color on the
     monitor and get key strokes.
 
+    :Example:
+
+        >>> mon = Monitor()
+        >>> mon.setColor("#00FF00FF")
+
     """
 
     def __init__(self, psychopy_win=None):
         self.psychopy_win = psychopy_win
         if not psychopy_win:
-            self.psychopy_win = visual.Window(size=(800,600))
+            self.psychopy_win = visual.Window(size=(800, 600))
         self.patch_stim = visual.PatchStim(self.psychopy_win, tex=None,
                 size=(2, 2), color=0, colorSpace="rgb255")
-        self.e = E() # small object storing one key
+        self.e = Event() # small object storing one key
 
     def setColor(self, psychopy_color, colorSpace="rgb255"):
         """
